6dc5aad3829fb28b21b66252026944d6779143a3
stijn-robben <stijn.robben2005@gmail.com>
2025-08-26 19:07:02 +0200

    .

--- AvansMaaltijdreservering.Core.DomainService/Interfaces/IPackageLockService.cs
+++
@@ -1,7 +0,0 @@
-namespace AvansMaaltijdreservering.Core.DomainService.Interfaces;
-
-public interface IPackageLockService
-{
-    Task<T> ExecuteWithPackageLockAsync<T>(int packageId, Func<Task<T>> operation);
-    Task ExecuteWithPackageLockAsync(int packageId, Func<Task> operation);
-}
